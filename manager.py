4468b74a25882c1f6225bd5e94fa55eaf34ed707
Ben Broderick Phillips <ben.phillips@nebula.com>
2014-01-11 13:38:12 -0800

    Run multiple git commands on any number of repos

--- manager.py
+++ manager.py
@@ -1,37 +1,68 @@
 #!/usr/bin/python
 
-import subprocess as sproc
+import subprocess
 import os
 from argparse import ArgumentParser
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('top_dir', nargs='?', default=os.getcwd())
-    return parser.parse_args()
+    parser.add_argument('args', nargs='+',
+                       help='Enter any number of git commands and directories')
+    args = parser.parse_args()
+    paths, commands = [], []
+    for a in args.args:
+        if os.path.isdir(a):
+            paths.append(a)
+        else:
+            commands.append(a.split(' '))
+    if not paths:
+        paths.append(os.getcwd())
+    return (commands, paths)
 
 
-def find_git_repos(dir):
+
+def find_git_repos(path):
     git_repos = []
-    if not os.path.isdir(dir):
+    if not os.path.isdir(path):
         return False
-    os.chdir(dir)
+    os.chdir(path)
     try:
-        sproc.check_output(["git rev-parse"], shell=True, stderr=sproc.PIPE)
-        return [dir]
-    except sproc.CalledProcessError:
+        subprocess.check_output(["git rev-parse"], shell=True, stderr=subprocess.PIPE)
+        return [path]
+    except subprocess.CalledProcessError:
         pass
-    for d in os.listdir(dir):
-        d = os.path.join(dir, d)
+    for d in os.listdir(path):
+        d = os.path.join(path, d)
         repos = find_git_repos(d)
         if repos:
             git_repos = git_repos + repos
     return git_repos
 
 
+def exec_git_command(path, cmd):
+    path = os.path.abspath(path)
+    git_repos = find_git_repos(path)
+    for repo in git_repos:
+        os.chdir(repo)
+        print "..." + repo.replace(path, '')
+        try:
+            print subprocess.check_output(["git"] + cmd)
+        except:
+            print 'FAILURE'
+            pass
+
+
 def main():
-    args = parse_args()
-    print find_git_repos(args.top_dir)
+    (commands, paths) = parse_args()
+    #find_git_repos(args.top_dir)
+    print commands, paths
+    for cmd in commands:
+        print "*"*80
+        print "Running 'git " + ' '.join(cmd) + "' in:"
+        print "*"*80
+        for path in paths:
+            exec_git_command(path, cmd)
 
 
 if __name__ == '__main__':
